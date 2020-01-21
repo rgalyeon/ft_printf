8d7fda1fea937cb9c775469754b30c50c76c2f06
rgalyeon <dmifilin@gmail.com>
2020-01-21 16:58:17 +0300

    Add: new norminette header with global variables

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,10 +6,9 @@
 /*   By: rgalyeon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/09 17:10:03 by rgalyeon          #+#    #+#             */
-/*   Updated: 2020/01/19 19:27:17 by rgalyeon         ###   ########.fr       */
+/*   Updated: 2020/01/21 16:51:16 by rgalyeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
@@ -17,122 +16,138 @@
 # include <stdarg.h>
 # include "libft.h"
 
-/**
+/*
 ** Structure for printf's placeholder
 ** Example: %[flag][width][.precision][length]type
 */
 
-typedef u_int8_t t_bool;
+typedef u_int8_t	t_bool;
 
 # define TRUE 1
 # define FALSE 0
 
-typedef struct	s_ph
+typedef struct		s_ph
 {
-	u_int8_t flag;
-	long long int width;
-	int precision;
-	u_int8_t length;
-	u_int8_t type;
-}				t_ph;
+	u_int8_t		flag;
+	long long int	width;
+	int				precision;
+	u_int8_t		length;
+	u_int8_t		type;
+}					t_ph;
 
-/**
+/*
 ** Flags field
 ** https://en.wikipedia.org/wiki/Printf_format_string
 */
 
-typedef struct s_flag
+typedef struct		s_flag
 {
 	char		symbol;
-	u_int8_t 	code;
-}				t_flag;
+	u_int8_t	code;
+}					t_flag;
 
-# define MINUS	(t_flag){'-', 0b00000001u}
-# define PLUS	(t_flag){'+', 0b00000010u}
-# define SPACE	(t_flag){' ', 0b00000100u}
-# define ZERO	(t_flag){'0', 0b00001000u}
-# define HASH	(t_flag){'#', 0b00010000u}
+static t_flag g_flag[] =
+{
+	{'-', 0b00000001u},
+	{'+', 0b00000010u},
+	{' ', 0b00000100u},
+	{'0', 0b00001000u},
+	{'#', 0b00010000u},
+};
 
-# define FLAGS(i) ((t_flag[5]){MINUS, PLUS, SPACE, ZERO, HASH})[i]
+# define MINUS	0
+# define PLUS	1
+# define SPACE	2
+# define ZERO	3
+# define HASH	4
 
-/**
-**
-** @param format
-** @param ...
-** @return
+/*
+** Length field
 */
 
-# define LENGTH_LONG_LONG			0b00000001u
-# define LENGTH_LONG				0b00000010u
-# define LENGTH_SHORT				0b00000100u
-# define LENGTH_CHAR				0b00001000u
-# define LENGTH_LONG_LONG_DOUBLE	0b00010000u
+# define LENGTH_LONG_LONG			0x1u
+# define LENGTH_LONG				0x2u
+# define LENGTH_SHORT				0x4u
+# define LENGTH_CHAR				0x8u
+# define LENGTH_LONG_LONG_DOUBLE	0x10u
 
-# define _L		(t_flag){'l', LENGTH_LONG}
-# define _S		(t_flag){'h', LENGTH_SHORT}
-# define _LLD	(t_flag){'L', LENGTH_LONG_LONG_DOUBLE}
+static t_flag		g_length[] =
+{
+	{'l', LENGTH_LONG},
+	{'h', LENGTH_SHORT},
+	{'L', LENGTH_LONG_LONG_DOUBLE}
 
-# define LENGTH(i) ((t_flag[3]){_L, _S, _LLD})[i]
+};
 
-# define TYPE		"diouxXcsp"
-# define N_TYPES	9
+/*
+** Defines for type
+*/
 
-typedef char *(*t_ptr)(t_vec **vec, t_ph *placeholder, va_list arg_ptr);
+typedef struct		s_type
+{
+	char			type;
+	void			(*function)(t_vec **, t_ph *placeholder, va_list args);
+}					t_type;
 
-# define _DI processing_di
-# define _X processing_x
-# define _U processing_u
-# define _C processing_c
-# define _STR processing_s
-# define _P processing_p
+# define TYPE	"diouxXcsp"
+# define CHAR	6
 
-# define CHAR 6
-
-# define _PROCESSING_TYPE(i) {_DI, _DI, _X, _U, _X, _X, _C, _STR, _P}[i]
-# define PROCESSING_TYPE(i, v, ph, va) (t_ptr[9])_PROCESSING_TYPE(i)(v, ph, va)
-
-/**
+/*
 ** Parameters for alignment
 */
 
-# define FULL_SIZE	align_params[0]
-# define ZERO_COUNT	align_params[1]
-# define PADDING	align_params[2]
+# define FULL_SIZE	0
+# define ZERO_PAD	1
+# define PAD		2
 
+/*
+** Main functions
+*/
 
-
-int				ft_printf(const char *format, ...);
-int				ft_vfprintf(int fd, const char *format, va_list arg_ptr);
-void			parse_placeholder(t_vec **vec, char **format, int *size,
+int					ft_printf(const char *format, ...);
+int					ft_vfprintf(int fd, const char *format, va_list arg_ptr);
+void				parse_placeholder(t_vec **vec, char **format, int *size,
 															va_list arg_ptr);
-void			processing_types(t_vec **vec, t_ph *placeholder, va_list
-arg_ptr);
+void				processing_types(t_vec **vec, t_ph *placeholder, va_list
+																	arg_ptr);
 
-/**
+/*
 ** Types processing functions
 */
 
-char			*processing_di(t_vec **vec, t_ph *placeholder, va_list arg_ptr);
-char			*processing_u(t_vec **vec, t_ph *placeholder, va_list arg_ptr);
-char			*processing_x(t_vec **vec, t_ph *placeholder, va_list arg_ptr);
-char			*processing_c(t_vec **vec, t_ph *placeholder, va_list arg_ptr);
-char			*processing_s(t_vec **vec, t_ph *placeholder, va_list arg_ptr);
-char			*processing_p(t_vec **vec, t_ph *placeholder, va_list arg_ptr);
+void				processing_di(t_vec **vec, t_ph *pholder, va_list arg_ptr);
+void				processing_u(t_vec **vec, t_ph *pholder, va_list arg_ptr);
+void				processing_x(t_vec **vec, t_ph *pholder, va_list arg_ptr);
+void				processing_c(t_vec **vec, t_ph *pholder, va_list arg_ptr);
+void				processing_s(t_vec **vec, t_ph *pholder, va_list arg_ptr);
+void				processing_p(t_vec **vec, t_ph *pholder, va_list arg_ptr);
 
-/**
+static t_type		g_type[] =
+{
+	{'d', processing_di},
+	{'i', processing_di},
+	{'o', processing_x},
+	{'u', processing_u},
+	{'x', processing_x},
+	{'X', processing_x},
+	{'c', processing_c},
+	{'s', processing_s},
+	{'p', processing_p},
+};
+
+/*
 ** Utils for project
 */
 
-__int128		max3(__int128 a, __int128 b, __int128 c);
-__int128		max2(__int128 a, __int128 b);
-int				ft_uint_len(__int128 nb);
-char			*ft_itoa_base(__int128 val, int base);
+__int128			max2(__int128 a, __int128 b);
+int					ft_uint_len(__int128 nb);
+char				*ft_itoa_base(__int128 val, int base);
 
-/**
+/*
 ** Utils for development
 ** TODO: delete when project is done
-**/
+*/
 
-void	print_placeholder(t_ph *placeholder);
+void				print_placeholder(t_ph *placeholder);
 
 #endif
